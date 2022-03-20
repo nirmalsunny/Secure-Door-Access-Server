e6fee593be9d2725a17bdedde9deb9622e953aa6
Nirmal <nirmal.sunny@study.beds.ac.uk>
2022-03-20 21:18:34 +0000

    changed routes to seperate file

--- system/Routes.php
+++ system/Routes.php
@@ -2,42 +2,11 @@
 
 class Routes
 {
-    protected $routes = [
-        "home" => "index@Home",
-        "error" => "error@Home",
-        "login" => "login@Home",
-        "register" => "register@Home",
-
-        "api/doors/all" => "all@Door",
-        "api/doors/add" => "add@Door",
-        "api/doors/delete" => "delete@Door",
-        "api/doors/edit" => "edit@Door",
-
-        "api/users/add" => "add@User",
-        "api/users/edit" => "edit@User",
-        "api/users/activate" => "activate@User",
-        "api/users/delete" => "delete@User",
-
-        "api/cards/all" => "all@Card",
-        "api/cards/add" => "add@Card",
-        "api/cards/update" => "update@Card",
-        "api/cards/delete" => "delete@Card",
-
-        "api/levels/all" => "all@Level",
-        "api/levels/add" => "add@Level",
-        "api/levels/delete" => "delete@Level",
-
-        "api/dashboard" => "dashboard@Home",
-
-        "api/logs/all" => "all@Log",
-        "api/logs/investigate" => "investigate@Log",
-
-        "api/access/all" => "all@Access"
-
-    ];
+    protected $routes;
 
     protected function routeExists($route)
     {
+        $this->routes = include ROOT . '/routes.php';
         if (array_key_exists($route, $this->routes)) {
             return true;
         } else {
