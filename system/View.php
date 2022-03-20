6cd0e9342ca6764dcadf456a5fd9a8d36e3b733c
Nirmal <nirmal.sunny@study.beds.ac.uk>
2022-03-20 21:54:11 +0000

    added header

--- system/View.php
+++ system/View.php
@@ -5,6 +5,7 @@ class View {
     public function __construct($template, $data) {
         if (file_exists(VIEWS_FOLDER . '/' . $template . '.php')) {
             is_null($data) ?: extract($data);
+            header('Content-Type: text/html');
             include VIEWS_FOLDER . '/' . $template . '.php';
         }
     }
