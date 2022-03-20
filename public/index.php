9974b50249fb62d387625746c667f2d928e08930
Nirmal <nirmal.sunny@study.beds.ac.uk>
2022-03-20 23:21:44 +0000

    moved vendor directory inside system

--- public/index.php
+++ public/index.php
@@ -4,7 +4,7 @@ define('APP_VERSION', '1.0.0');
 
 include dirname(__FILE__).'/../config.php';
 
-include ROOT . '/vendor/autoload.php';
+include SYSTEM . '/vendor/autoload.php';
 include SYSTEM . '/Bootstrap.php';
 include SYSTEM . '/Routes.php';
 include SYSTEM . '/Controller.php';
